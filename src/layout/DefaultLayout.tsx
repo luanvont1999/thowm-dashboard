3f7caf77d7ddf60a3a59aa68f6255244aecc204c
LuanVo <vtluan.nt.1999@gmail.com>
2023-03-13 18:00:18 +0700

    init source

---
+++ src/layout/DefaultLayout.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const defaultLayout = () => {}
+
+export default defaultLayout
---
+++ src/utils/index.tsx
@@ -0,0 +1,7 @@
+export const test = () => {
+  console.log('aasd')
+}
+
+export default {
+  test,
+}
